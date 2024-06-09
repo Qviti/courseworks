deb2e193bfbd9e96066d464291f68799811ed2e4
Qviti <149914714+Qviti@users.noreply.github.com>
2024-06-10 01:51:02 +0400

    2nd commit, bug fixes

---
+++ RESIT/src/Appetizer.java
@@ -0,0 +1,5 @@
+public class Appetizer extends FoodItem {
+    public Appetizer(int itemId, String name, double price, String description) {
+        super(itemId, name, "Appetizer", price, description);
+    }
+}
---
+++ RESIT/src/MobileUI.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class MobileUI implements UI {
+    @Override
+    public void displayFoodItems(List<FoodItem> items) {
+        // ToDo
+    }
+
+    @Override
+    public void displayCart(Cart cart) {
+        // ToDo
+    }
+
+    @Override
+    public void displayOrderStatus(Order order) {
+        // ToDo
+    }
+
+    @Override
+    public void displayNotification(Notification notification) {
+        // ToDo
+    }
+}
+
